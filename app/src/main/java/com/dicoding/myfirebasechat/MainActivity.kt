4f1c8c6234f4394aa939a917a2a32000dc353734
Luthfi Aji <luthfi.ajiw@gmail.com>
2023-01-17 09:56:21 +0700

    message adapter

--- app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
+++ app/src/main/java/com/dicoding/myfirebasechat/MainActivity.kt
@@ -4,15 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.myfirebasechat.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +26,31 @@ class MainActivity : AppCompatActivity() {
 
         auth = Firebase.auth
         val firebaseUser = auth.currentUser
-
         if (firebaseUser == null) {
             val loginIntent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(loginIntent)
             finish()
             return
+        }
+
+        db = Firebase.database
+        val messageRef = db.reference.child(MESSAGES_CHILD)
+        binding.sendButton.setOnClickListener {
+            val friendlyMessage = MessageModel(
+                binding.messageEditText.text.toString(),
+                firebaseUser.displayName.toString(),
+                firebaseUser.photoUrl.toString(),
+                Date().time
+            )
+
+            messageRef.push().setValue(friendlyMessage) { error, _ ->
+                if (error != null) {
+                    Toast.makeText(this, getString(R.string.send_error) + error.message, Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, getString(R.string.send_success), Toast.LENGTH_SHORT).show()
+                }
+            }
+            binding.messageEditText.text.clear()
         }
     }
 
@@ -50,5 +74,9 @@ class MainActivity : AppCompatActivity() {
         auth.signOut()
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
+    }
+
+    companion object {
+        const val MESSAGES_CHILD = "messages"
     }
 }
---
+++ app/src/main/java/com/dicoding/myfirebasechat/MessageModel.kt
@@ -0,0 +1,8 @@
+package com.dicoding.myfirebasechat
+
+data class MessageModel(
+    val text: String? = null,
+    val name: String? = null,
+    val photoUrl: String? = null,
+    val timestamp: Long? = null
+)
